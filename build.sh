28d1d3f4e01b674ab31cbfa76fd462a3cca6f94c
wulan17 <galihgustip@gmail.com>
2020-02-09 02:29:03 +0700

    size

--- build.sh
+++ build.sh
@@ -19,7 +19,8 @@ git remote add fetch $repo2
 git remote add origin $repo1
 git fetch $repo2 $branch2
 git checkout FETCH_HEAD
-git checkout -b $branch1
+sudo du -hs .
+#git checkout -b $branch1
 #git fetch fetch $branch1
 #git cherry-pick 336d54d5c8bee7c8ca2ea74488f99e9f3e58d65c b97ed13a90463aae6e116d56d2f9023f201e4ceb 9bcc8b85c51fd0b838d83ab71f758fa9a64028fc 5d8df133d8431b0a582d89cd2c3ea9d9fa5b2b29 b78c3b57160004c54144228275626775e72beed5 87bad57ae9d956ede96fc7cc0c4937e8b6f92157 549bf2a78e248913555ac0ae5f6be1b1c6f5587b --signoff
-git push -u -f origin $branch1
+#git push -u -f origin $branch1
